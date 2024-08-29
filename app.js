3d0172d0bc13ac2386f86b64e48f1945ae3faecd
mamodzilla <r0kkapon@yandex.ru>
2024-08-29 03:19:27 +0300

    feat: add initial .js file of Node.js app

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+http.createServer(function(request,response){
+     
+    response.end("Hello NodeJS!");
+     
+}).listen(3000, "127.0.0.1",function(){
+    console.log("The server has started listening for requests on port 3000");
+});
